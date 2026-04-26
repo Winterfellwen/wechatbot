ec62fbb9a02ada15ebff1e2873ecc369c0585551
Winterfellwen <Winterfellwen@outlook.com>
2026-04-27 01:06:43 +0800

    Fix: restore loginWithCode function

--- pages/index/index.js
+++ pages/index/index.js
@@ -49,6 +49,79 @@ login() {
     }
   },
 
+  loginWithCode(code, avatarUrl) {
+    wx.request({
+      url: `${API_URL}/api/wechat/openid?code=${code}`,
+      method: 'GET',
+      success: (res) => {
+        wx.hideLoading();
+        if (res.data && res.data.openid) {
+          const openid = res.data.openid;
+          this.checkOrCreateUser(openid, avatarUrl);
+        } else {
+          wx.showToast({ title: '登录失败', icon: 'none' });
+        }
+      },
+      fail: () => {
+        wx.hideLoading();
+        wx.showToast({ title: '网络错误', icon: 'none' });
+      }
+    });
+  },
+
+  checkOrCreateUser(openid, avatarUrl) {
+    wx.request({
+      url: `${API_URL}/api/user/check`,
+      method: 'POST',
+      data: { openid },
+      header: { 'x-api-key': API_KEY },
+      success: (res) => {
+        if (res.data && res.data.exists) {
+          const userInfo = res.data.user;
+          app.setUserInfo(userInfo);
+          this.setData({ userInfo, isLoggedIn: true });
+        } else {
+          this.promptNickname(openid, avatarUrl);
+        }
+      },
+      fail: () => {
+        wx.showToast({ title: '网络错误', icon: 'none' });
+      }
+    });
+  },
+
+  promptNickname(openid, avatarUrl) {
+    wx.showModal({
+      title: '设置昵称',
+      editable: true,
+      placeholderText: '请输入昵称',
+      success: (res) => {
+        if (res.content && res.content.trim()) {
+          this.createUser(openid, avatarUrl, res.content.trim());
+        } else {
+          wx.showToast({ title: '昵称不能为空', icon: 'none' });
+        }
+      }
+    });
+  },
+
+  createUser(openid, avatarUrl, nickName) {
+    wx.request({
+      url: `${API_URL}/api/user/create`,
+      method: 'POST',
+      data: { openid, nickName, avatarUrl },
+      header: { 'x-api-key': API_KEY },
+      success: (res) => {
+        if (res.data && res.data.user) {
+          const userInfo = res.data.user;
+          app.setUserInfo(userInfo);
+          this.setData({ userInfo, isLoggedIn: true });
+          wx.showToast({ title: '登录成功', icon: 'success' });
+        }
+      }
+    });
+  },
+
   handleEntryTap(e) {
     const type = e.currentTarget.dataset.type;
     if (type === 'developing') {
