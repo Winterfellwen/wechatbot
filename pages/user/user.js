8d59ab32a1dc707f001ee3dac8300448c52acd61
Winterfellwen <Winterfellwen@outlook.com>
2026-04-27 00:35:59 +0800

    User page: login directly instead of redirect to index

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,4 +1,6 @@
 const app = getApp();
+const API_URL = 'https://wechatbot-api.onrender.com';
+const API_KEY = 'rnd_cIEZYlFoB5pJx4byk0tiONKcCBnk';
 
 Page({
   data: {
@@ -25,8 +27,68 @@ Page({
 
   handleUserTap() {
     if (!this.data.isLoggedIn) {
-      wx.switchTab({ url: '/pages/index/index' });
+      this.login();
     }
+  },
+
+  login() {
+    wx.showLoading({ title: '登录中...' });
+    
+    wx.login({
+      success: (res) => {
+        if (res.code) {
+          this.loginWithCode(res.code);
+        } else {
+          wx.hideLoading();
+          wx.showToast({ title: '登录失败', icon: 'none' });
+        }
+      },
+      fail: () => {
+        wx.hideLoading();
+        wx.showToast({ title: '登录失败', icon: 'none' });
+      }
+    });
+  },
+
+  loginWithCode(code) {
+    const OPENID = wx.getStorageSync('openid') || 'wx_' + Date.now() + '_' + Math.random().toString(36).substr(2, 9);
+    wx.setStorageSync('openid', OPENID);
+
+    wx.request({
+      url: `${API_URL}/api/users/${OPENID}/wx-login`,
+      method: 'POST',
+      header: {
+        'Authorization': API_KEY,
+        'Content-Type': 'application/json'
+      },
+      data: { code: code },
+      success: (res) => {
+        wx.hideLoading();
+        if (res.data && res.data.user) {
+          const userInfo = {
+            openid: res.data.user.openid,
+            nickName: res.data.user.nickname || '微信用户',
+            avatarUrl: res.data.user.avatarurl || ''
+          };
+          app.setUserInfo(userInfo);
+          this.setData({ userInfo: userInfo, isLoggedIn: true });
+          wx.showToast({ title: '登录成功', icon: 'success' });
+        } else {
+          this.simpleLogin(OPENID);
+        }
+      },
+      fail: () => {
+        wx.hideLoading();
+        this.simpleLogin(OPENID);
+      }
+    });
+  },
+
+  simpleLogin(openid) {
+    const userInfo = { openid: openid, nickName: '微信用户', avatarUrl: '' };
+    app.setUserInfo(userInfo);
+    this.setData({ userInfo: userInfo, isLoggedIn: true });
+    wx.showToast({ title: '登录成功', icon: 'success' });
   },
 
   handleChooseAvatar(e) {
@@ -36,8 +98,20 @@ Page({
       const userInfo = { ...this.data.userInfo, avatarUrl: avatarUrl };
       app.setUserInfo(userInfo);
       this.setData({ userInfo: userInfo });
+      this.saveUserToBackend(userInfo);
       wx.showToast({ title: '头像已更新', icon: 'success' });
     }
+  },
+
+  saveUserToBackend(userInfo) {
+    if (!userInfo.openid) return;
+    wx.request({
+      url: `${API_URL}/api/users/${userInfo.openid}`,
+      method: 'POST',
+      header: { 'Authorization': API_KEY, 'Content-Type': 'application/json' },
+      data: { nickName: userInfo.nickName, avatarUrl: userInfo.avatarUrl },
+      fail: () => {}
+    });
   },
 
   handleLogout() {
