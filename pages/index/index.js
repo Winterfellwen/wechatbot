82a1af6f32ce0359b88d394cd261eb3373d80503
Winterfellwen <Winterfellwen@outlook.com>
2026-04-27 01:05:19 +0800

    Fix: use view like user page, remove button

--- pages/index/index.js
+++ pages/index/index.js
@@ -25,7 +25,7 @@ Page({
     });
   },
 
-  login() {
+login() {
     wx.showLoading({ title: '登录中...' });
     wx.login({
       success: (res) => {
@@ -43,117 +43,10 @@ Page({
     });
   },
 
-  handleChooseAvatar(e) {
-    const avatarUrl = e.detail.avatarUrl;
-    if (avatarUrl) {
-      if (!this.data.isLoggedIn) {
-        this.loginWithAvatar(avatarUrl);
-      } else {
-        const userInfo = { ...this.data.userInfo, avatarUrl: avatarUrl };
-        app.setUserInfo(userInfo);
-        this.setData({ userInfo: userInfo });
-        wx.showToast({ title: '头像已更新', icon: 'success' });
-      }
+  handleUserTap() {
+    if (!this.data.isLoggedIn) {
+      this.login();
     }
-  },
-
-  loginWithAvatar(avatarUrl) {
-    wx.showLoading({ title: '登录中...' });
-    wx.login({
-      success: (res) => {
-        if (res.code) {
-          this.loginWithCode(res.code, avatarUrl);
-        } else {
-          wx.hideLoading();
-          wx.showToast({ title: '登录失败', icon: 'none' });
-        }
-      },
-      fail: () => {
-        wx.hideLoading();
-        wx.showToast({ title: '登录失败', icon: 'none' });
-      }
-    });
-  },
-
-  loginWithCode(code, avatarUrl) {
-    wx.request({
-      url: `${API_URL}/api/wechat/openid?code=${code}`,
-      method: 'GET',
-      success: (res) => {
-        wx.hideLoading();
-        if (res.data && res.data.openid) {
-          wx.setStorageSync('openid', res.data.openid);
-          this.loginWithOpenid(res.data.openid, avatarUrl);
-        } else {
-          this.loginAfterSuccess({ openid: 'wx_' + Date.now() }, avatarUrl);
-        }
-      },
-      fail: () => {
-        wx.hideLoading();
-        this.loginAfterSuccess({ openid: 'wx_' + Date.now() }, avatarUrl);
-      }
-    });
-  },
-
-  loginWithOpenid(openid, avatarUrl) {
-    wx.request({
-      url: `${API_URL}/api/users/${openid}/wx-login`,
-      method: 'POST',
-      header: { 'Authorization': API_KEY, 'Content-Type': 'application/json' },
-      data: {},
-      success: (res) => {
-        this.loginAfterSuccess(res.data?.user || { openid: openid }, avatarUrl);
-      },
-      fail: () => {
-        this.loginAfterSuccess({ openid: openid }, avatarUrl);
-      }
-    });
-  },
-
-  loginAfterSuccess(userData, avatarUrl) {
-    const userInfo = {
-      openid: userData.openid,
-      nickName: userData.nickname || '',
-      avatarUrl: avatarUrl || userData.avatarurl || ''
-    };
-    
-    if (!userInfo.nickName) {
-      this.showNicknameInput(userInfo);
-    } else {
-      this.completeLogin(userInfo);
-    }
-  },
-
-  showNicknameInput(userInfo) {
-    wx.showModal({
-      title: '设置昵称',
-      placeholderText: '请输入昵称',
-      editable: true,
-      success: (res) => {
-        if (res.confirm && res.content && res.content.trim()) {
-          userInfo.nickName = res.content.trim();
-          this.saveAndCompleteLogin(userInfo);
-        } else {
-          this.showNicknameInput(userInfo);
-        }
-      },
-      fail: () => {
-        userInfo.nickName = '用户' + Date.now() % 10000;
-        this.saveAndCompleteLogin(userInfo);
-      }
-    });
-  },
-
-  saveAndCompleteLogin(userInfo) {
-    app.setUserInfo(userInfo);
-    this.setData({ userInfo: userInfo, isLoggedIn: true });
-    wx.showToast({ title: '登录成功', icon: 'success' });
-  },
-
-  completeLogin(userInfo) {
-    app.setUserInfo(userInfo);
-    this.setData({ userInfo: userInfo, isLoggedIn: true });
-    wx.showToast({ title: '登录成功', icon: 'success' });
   },
 
   handleEntryTap(e) {
