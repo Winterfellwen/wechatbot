fb7c0b5af94c870e609db8edbf49313f38b00537
user <user@example.com>
2026-04-27 11:47:15 +0800

    Refactor: comprehensive japanese curriculum N5→N1 with 1231 words, 264 grammar points, 304 exercises

---
+++ japanese/data/words/index.js
@@ -0,0 +1,22 @@
+const wordsN5 = require('./words/words_n5.js');
+const wordsN4 = require('./words/words_n4.js');
+const wordsN3 = require('./words/words_n3.js');
+const wordsN2 = require('./words/words_n2.js');
+const wordsN1 = require('./words/words_n1.js');
+
+const allWords = [...wordsN5, ...wordsN4, ...wordsN3, ...wordsN2, ...wordsN1];
+
+module.exports = {
+  all: allWords,
+  byLesson(lesson) {
+    return allWords.filter(w => w.lesson === lesson);
+  },
+  byLevel(level) {
+    return allWords.filter(w => w.level === level);
+  },
+  n5: wordsN5,
+  n4: wordsN4,
+  n3: wordsN3,
+  n2: wordsN2,
+  n1: wordsN1
+};
