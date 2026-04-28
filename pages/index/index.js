a6e37cb1015ac1f1d82095d87f1b61c5e5bf84b3
user <user@example.com>
2026-04-29 01:06:49 +0800

    add Word editor entry card, gray out german learning

--- pages/index/index.js
+++ pages/index/index.js
@@ -127,6 +127,8 @@ login() {
       wx.navigateTo({ url: '/japanese/pages/learn/learn' });
     } else if (type === 'german') {
       wx.showToast({ title: '德语学习开发中', icon: 'none' });
+    } else if (type === 'word') {
+      wx.showToast({ title: 'Word编辑开发中', icon: 'none' });
     } else if (type === 'pdf') {
       wx.navigateTo({ url: '/pdf/pages/index/index' });
     } else if (type === 'developing') {
