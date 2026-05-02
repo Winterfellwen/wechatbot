4bb680d4eaced73892ace823c1a5b6f9427ed972
user <user@example.com>
2026-05-03 00:01:36 +0800

    refactor: reorganize course page with level and unit selection

--- german/pages/course/course.js
+++ german/pages/course/course.js
@@ -4,34 +4,87 @@ const a1Vocab = require('../../data/a1/vocab.js');
 const a1Grammar = require('../../data/a1/grammar.js');
 const a1Pronunciation = require('../../data/a1/pronunciation.js');
 const a1Texts = require('../../data/a1/texts.js');
+const a2Vocab = require('../../data/a2/vocab.js');
+
+const levelData = {
+  a1: {
+    name: 'A1 基础',
+    vocab: a1Vocab,
+    grammar: a1Grammar,
+    pronunciation: a1Pronunciation,
+    texts: a1Texts,
+    units: 15
+  },
+  a2: {
+    name: 'A2 进阶',
+    vocab: a2Vocab,
+    grammar: [],
+    pronunciation: [],
+    texts: [],
+    units: 15
+  }
+};
 
 Page({
   data: {
-    currentTab: 'vocab',
-    level: 'a1',
+    currentLevel: 'a1',
+    levels: [
+      { id: 'a1', name: 'A1 基础', units: 15 },
+      { id: 'a2', name: 'A2 进阶', units: 15 },
+      { id: 'b1', name: 'B1 中级', units: 15 },
+      { id: 'b2', name: 'B2 高级', units: 15 }
+    ],
+    currentUnit: 1,
+    showUnitSelector: false,
     vocabList: [],
     grammarList: [],
     pronunciationList: [],
-    textList: [],
-    searchKeyword: ''
+    textList: []
   },
 
-  onLoad: function(options) {
-    this.loadData();
+  onLoad: function() {
+    this.loadLevelData();
   },
 
-  loadData: function() {
+  onShow: function() {
+    this.loadLevelData();
+  },
+
+  loadLevelData: function() {
+    const level = levelData[this.data.currentLevel];
+    if (level) {
+      const startIdx = (this.data.currentUnit - 1) * 15;
+      const endIdx = startIdx + 15;
+      
+      this.setData({
+        vocabList: level.vocab.slice(startIdx, endIdx),
+        grammarList: level.grammar,
+        pronunciationList: level.pronunciation,
+        textList: level.texts
+      });
+    }
+  },
+
+  selectLevel: function(e) {
+    const levelId = e.currentTarget.dataset.id;
     this.setData({
-      vocabList: a1Vocab,
-      grammarList: a1Grammar,
-      pronunciationList: a1Pronunciation,
-      textList: a1Texts
+      currentLevel: levelId,
+      currentUnit: 1
     });
+    this.loadLevelData();
   },
 
-  switchTab: function(e) {
-    const tab = e.currentTarget.dataset.tab;
-    this.setData({ currentTab: tab });
+  selectUnit: function(e) {
+    const unit = e.currentTarget.dataset.unit;
+    this.setData({
+      currentUnit: unit,
+      showUnitSelector: false
+    });
+    this.loadLevelData();
+  },
+
+  toggleUnitSelector: function() {
+    this.setData({ showUnitSelector: !this.data.showUnitSelector });
   },
 
   playAudio: function(e) {
@@ -43,18 +96,6 @@ Page({
     const word = e.currentTarget.dataset.word;
     storage.addToWordBook(word);
     wx.showToast({ title: '已加入生词本', icon: 'success' });
-  },
-
-  onSearch: function(e) {
-    const keyword = e.detail.value;
-    this.setData({ searchKeyword: keyword });
-  },
-
-  goToLesson: function(e) {
-    const index = e.currentTarget.dataset.index;
-    wx.navigateTo({
-      url: `/german/pages/lesson/lesson?index=${index}`
-    });
   },
 
   onShareAppMessage: function() {
